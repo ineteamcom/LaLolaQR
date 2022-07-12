41b915618da218040acccbc714d584b9815d257a
ineteamcom <ineteam@gmail.com>
2022-07-12 15:55:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbyyz2FVpcr8jqQ4N5hdS1VnJETib7JDFMFJkcrxq6j0m6lfFTLiQ7sv9ycPyX6aYgt8/exec";
